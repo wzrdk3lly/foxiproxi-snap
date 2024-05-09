90f8752d2926c3f9b48e10fabb325b1d0a073a23
wzrdk3lly <dkmk1149@gmail.com>
2024-05-08 22:16:17 -0400

    ADD UI for RPC Request Modifcation

--- packages/snap/src/index.ts
+++ packages/snap/src/index.ts
@@ -1,29 +1,65 @@
 import type { OnTransactionHandler, OnSignatureHandler,  } from '@metamask/snaps-sdk';
-import { panel,heading, text, copyable, SeverityLevel } from '@metamask/snaps-sdk';
+import { panel,heading, text, copyable, SeverityLevel, button , input, form, ButtonType, divider} from '@metamask/snaps-sdk';
 
-
+// All the on chain transactions - an eth_sendTransaction rpc request 
 export const onTransaction: OnTransactionHandler = async ({
   transaction,
   transactionOrigin,
 }) => {
 
   const transactionString = JSON.stringify(transaction)
+
+  const interfaceId = await snap.request({
+    method: "snap_createInterface",
+    params: {
+      ui:  panel([
+        text("Intercepted RPC Request"),
+        copyable(`{"method": "eth_sendTransaction", "params":[${transactionString}]}`),
+        divider(),
+        text("Modify RPC Request"),
+        // A top-level form...
+        form({
+          name: "modify-rpc-request",
+          children: [
+            // ...with a nested input.
+            input({
+              name: "to-address-modification",
+              placeholder: "Modify `to` address ",
+            }),
+            input({
+              name: "value-modification",
+              placeholder: "Modify request value  ",
+            }),
+            input({
+              name: "calldata-modification",
+              placeholder: "Modify calldata",
+            }),
+            button("Modify", ButtonType.Submit, "modify"),
+          ],
+        }),
+      ]),
+    },
+  });
+// I cant return both content and snap interface 
   return{
-    content: panel([
-      heading(` ${transactionOrigin}`),
-      text(`The raw transaction details are:`),
-      copyable(`{"method": "eth_sendTransaction", "params":[${transactionString}]}`) //formatted and made copyable for easier tx replay
-    ])
+    id: interfaceId
+    // content: panel([
+    //   heading(` ${transactionOrigin}`),
+    //   text(`The raw transaction details are:`),
+    //   copyable(`{"method": "eth_sendTransaction", "params":[${transactionString}]}`) //formatted and made copyable for easier tx replay
+    // ])
   }
 };
 
 
+
+// signedType_v4, signedType_v3, etc -- All the offchain signature request
 export const onSignature: OnSignatureHandler = async ({
   signature,
   signatureOrigin,
 }) => {
   const signatureString = JSON.stringify(signature)
-  
+
   return {
     content: panel([
       heading(`${signatureOrigin}`),
@@ -33,6 +69,8 @@ export const onSignature: OnSignatureHandler = async ({
     severity: SeverityLevel.Critical,
   };
 };
+
+
 
 
 
